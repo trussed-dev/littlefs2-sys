415e54b7269e7068a9d13eba0f8ca42119c95c41
Nicolas Stalder <n@stalder.io>
2019-12-11 05:50:58 +0100

    assertions and trace feature flags

--- build.rs
+++ build.rs
@@ -2,22 +2,31 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    cc::Build::new()
+    let mut builder = cc::Build::new();
+    let builder = builder
         .flag("-std=c11")
         .flag("-DLFS_NO_MALLOC")
-        .flag("-DLFS_NO_ASSERT")
         .flag("-DLFS_NO_DEBUG")
         .flag("-DLFS_NO_WARN")
         .flag("-DLFS_NO_ERROR")
         .file("littlefs/lfs.c")
         .file("littlefs/lfs_util.c")
         .file("string.c")
-        .compile("lfs-sys");
+    ;
+
+    #[cfg(not(feature = "assertions"))]
+    let builder = builder.flag("-DLFS_NO_ASSERT");
+
+    #[cfg(feature = "trace")]
+    let builder = builder.flag("-DLFS_YES_TRACE");
+
+    builder.compile("lfs-sys");
 
     let bindings = bindgen::Builder::default()
         .header("littlefs/lfs.h")
         .use_core()
         .ctypes_prefix("cty")
+        .rustfmt_bindings(true)
         .generate()
         .expect("Unable to generate bindings");
 
