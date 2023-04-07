2a4055a198a88e4ac989f1e7149af506fb999e9a
Sosthène Guédon <sosthene@nitrokey.com>
2023-04-07 10:56:10 +0200

    Bump littlefs version
    This patch:
    
    - Bumps the littlefs version to the latest released (v2.9.3)
    - Adds a "multiversion" feature flag that exposes the littlefs feature
    of the same name
    - Patch lfs.h before building to avoid errors from clang failing to find
    the string.h header (this step is not required for the compilation
    because compilation is done through gcc by default)

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut builder = cc::Build::new();
     let builder = builder
-        .flag("-std=c11")
+        .flag("-std=c99")
         .flag("-DLFS_NO_DEBUG")
         .flag("-DLFS_NO_WARN")
         .flag("-DLFS_NO_ERROR")
@@ -24,17 +24,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(not(feature = "malloc"))]
     builder.flag("-DLFS_NO_MALLOC");
 
+    #[cfg(feature = "multiversion")]
+    let builder = builder.flag("-DLFS_MULTIVERSION");
+
     builder.compile("lfs-sys");
 
+    // Patch lfs.h to remove the lfs_util import because clang fails to locate the
+    // libraries for the custom target (especially string.h)
+    // Compilation before that succeeds because it's using gcc,
+    // which comes as a distribution with these utils.
+    // Turns out lfs_utils is not used in lfs.h, and clang properly finds stdint.h and stdbool,
+    // but not string.h
+    let lfs_h = std::fs::read_to_string("littlefs/lfs.h").expect("Reading lfs.h succeeds");
+    println!("cargo::rerun-if-changed=littlefs/lfs.h");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_lfs_h = out_path.join("lfs.h");
+    std::fs::write(
+        &out_lfs_h,
+        lfs_h.replace(
+            r##"#include "lfs_util.h""##,
+            "#include <stdint.h>\n#include <stdbool.h>",
+        ),
+    )
+    .expect("Failed to write lfs.h");
+
     let bindings = bindgen::Builder::default()
-        .header("littlefs/lfs.h")
+        .header(out_lfs_h.into_os_string().into_string().unwrap())
+        .clang_arg("-std=c99")
+        .clang_arg("-DLFS_NO_DEBUG")
+        .clang_arg("-DLFS_NO_WARN")
+        .clang_arg("-DLFS_NO_ERROR")
         .use_core()
         .allowlist_item("lfs_.*")
         .allowlist_item("LFS_.*")
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
