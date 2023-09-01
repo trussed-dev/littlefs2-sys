9aa2fc8682b596c763a89dde137907aff130fcf3
Alex Bohm <aabohm7@gmail.com>
2023-08-31 23:06:37 -0500

    Updated bindgen to latest to support remapping of riscv triple naming.

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut builder = cc::Build::new();
-    let target = env::var("TARGET")?;
     let builder = builder
         .flag("-std=c11")
         .flag("-DLFS_NO_DEBUG")
@@ -11,8 +10,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .flag("-DLFS_NO_ERROR")
         .file("littlefs/lfs.c")
         .file("littlefs/lfs_util.c")
-        .file("string.c")
-    ;
+        .file("string.c");
 
     #[cfg(not(feature = "assertions"))]
     let builder = builder.flag("-DLFS_NO_ASSERT");
@@ -27,7 +25,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let bindings = bindgen::Builder::default()
         .header("littlefs/lfs.h")
-        .clang_arg(format!("--target={}", target))
         .use_core()
         .ctypes_prefix("cty")
         .rustfmt_bindings(true)
